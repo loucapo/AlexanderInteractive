87e06c498f0129edb9ac7175e46a290d3517907c
Lou Capozzoli <loucapo@yahoo.com>
2016-02-25 12:09:24 -0500

    refactored to make it all work better with a dynamic number of tabs, load tab data from a json and clean up the js and css a bit

--- scripts.js
+++ scripts.js
@@ -1,12 +1,28 @@
-$(document).ready(function () {
+$( document ).ready( function () {
 
-    if (location.hash) {
-        var hash = location.hash.replace('#!/', '');
-        console.log(hash);
-        $('[title=' + hash + ']').click();
-    }
+    $.getJSON( 'content.json', function( data ) {
+        var items = [], checks = [], tabs = [], content = [], i = 1;
 
-    $('.state').click(function () {
-        location.hash = '!/' + this.title;
+        $.each( data.tabs, function( key, val ) {
+            var checked = ( i === 1 ) ? 'checked' : '';
+
+            // split into multiple lines for legibility
+            items.push( '<input id="tab' + i + '" type="radio" name="grp" ' + checked + '/>' +
+                        '<label title="' + val.title + '" for="tab' + i + '">' + val.label + '</label>' +
+                        '<div class="content">' + val.content + '</div>' );
+            i++;
+        });
+
+        $( '.container' ).append( items.join('') );
+
+        $( 'label' ).click( function () {
+            location.hash = '!/' + this.title;
+        });
+
+        if ( location.hash ) {
+            var hash = location.hash.replace( '#!/', '' );
+            $( 'label[title=' + hash + ']' ).click();
+        }
     });
+
 });
